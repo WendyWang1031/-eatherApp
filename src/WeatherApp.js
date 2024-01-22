2353ab533f644734a4207ed3ae06cd293cd64397
PinYi Lee <pinyi_lee@icloud.com>
2024-01-22 16:30:02 +0800

    添加深色主題

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -7,6 +7,27 @@ import { ReactComponent as AirFlowIcon } from "./images/air-Flow.svg";
 import { ReactComponent as RainIcon } from "./images/rain.svg";
 import { ReactComponent as RedoIcon } from "./images/redo.svg";
 import { ReactComponent as LoadingIcon } from "./images/loading.svg";
+import { ThemeProvider } from "@emotion/react";
+
+const theme = {
+  Light: {
+    backgroundColor: "#ededed",
+    foregroundColor: "#f9f9f9",
+    boxShadow: "0 1px 3px 0 #999999",
+    titleColor: "#212121",
+    temperatureColor: "#757575",
+    textColor: "#828282",
+  },
+  dark: {
+    backgroundColor: "#1F2022",
+    foregroundColor: "#121416",
+    boxShadow:
+      "0 1px 4px 0 rgba(12, 12, 13, 0.2), 0 0 0 1px rgba(0, 0, 0, 0.15)",
+    titleColor: "#f9f9fa",
+    temperatureColor: "#dddddd",
+    textColor: "#cccccc",
+  },
+};
 
 const Cloudy = styled(CloudyIcon)`
   flex-basis: 30px;
@@ -19,7 +40,7 @@ const Refresh = styled.div`
   font-size: 12px;
   display: inline-flex;
   align-items: flex-end;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
 
   svg {
     margin-left: 10px;
@@ -40,7 +61,7 @@ const Refresh = styled.div`
 `;
 
 const Container = styled.div`
-  background-color: #ededed;
+  background-color: ${({ theme }) => theme.backgroundColor};
   height: 100%;
   display: flex;
   align-items: center;
@@ -50,21 +71,21 @@ const Container = styled.div`
 const WeatherCard = styled.div`
   position: relative;
   min-width: 360px;
-  box-shadow: 0 1px 3px 0 #999999;
-  background-color: #f9f9f9;
+  box-shadow: ${({ theme }) => theme.boxShadow};
+  background-color: ${({ theme }) => theme.foregroundColor};
   box-sizing: border-box;
   padding: 30px 15px;
 `;
 
 const Location = styled.div`
   font-size: 28px;
-  color: ${(props) => (props.theme === "dark" ? "#dadada" : "#212121")};
+  color: ${({ theme }) => theme.titleColor};
   margin-bottom: 20px;
 `;
 
 const Description = styled.div`
   font-size: 16px;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
   margin-bottom: 30px;
 `;
 
@@ -76,7 +97,7 @@ const CurrentWeather = styled.div`
 `;
 
 const Temperature = styled.div`
-  color: #757575;
+  color: ${({ theme }) => theme.temperatureColor};
   font-size: 96px;
   font-weight: 300;
   display: flex;
@@ -92,7 +113,7 @@ const AirFlow = styled.div`
   align-items: center;
   font-size: 16x;
   font-weight: 300;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
   margin-bottom: 20px;
   svg {
     width: 25px;
@@ -106,7 +127,7 @@ const Rain = styled.div`
   align-items: center;
   font-size: 16px;
   font-weight: 300;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
   svg {
     width: 25px;
     height: auto;
@@ -258,42 +279,44 @@ const WeatherApp = () => {
   }, [fetchData]);
 
   return (
-    <Container>
-      {console.log("render, is Loading: ", isLoading)}
-      <WeatherCard>
-        <Location>{stationName}</Location>
-        <Description>
-          {description}
-          {comfortability}
-        </Description>
-        <CurrentWeather>
-          <Temperature>
-            {Math.round(temperature)} <Celsius>°C</Celsius>
-          </Temperature>
-          <WeatherIcon
-            currentWeatherCode={weatherCode}
-            moment={moment || "day"}
-          />
-        </CurrentWeather>
-        <AirFlow>
-          <AirFlowIcon />
-          {windSpeed} m/h
-        </AirFlow>
-        <Rain>
-          <RainIcon />
-          {rainPossibility}%
-        </Rain>
+    <ThemeProvider theme={theme.dark}>
+      <Container>
+        {console.log("render, is Loading: ", isLoading)}
+        <WeatherCard>
+          <Location>{stationName}</Location>
+          <Description>
+            {description}
+            {comfortability}
+          </Description>
+          <CurrentWeather>
+            <Temperature>
+              {Math.round(temperature)} <Celsius>°C</Celsius>
+            </Temperature>
+            <WeatherIcon
+              currentWeatherCode={weatherCode}
+              moment={moment || "day"}
+            />
+          </CurrentWeather>
+          <AirFlow>
+            <AirFlowIcon />
+            {windSpeed} m/h
+          </AirFlow>
+          <Rain>
+            <RainIcon />
+            {rainPossibility}%
+          </Rain>
 
-        <Refresh onClick={fetchData} isLoading={isLoading}>
-          最後觀測時間:
-          {new Intl.DateTimeFormat("zh-TW", {
-            hour: "numeric",
-            minute: "numeric",
-          }).format(new Date(observationTime))}{" "}
-          {isLoading ? <LoadingIcon /> : <RedoIcon />}
-        </Refresh>
-      </WeatherCard>
-    </Container>
+          <Refresh onClick={fetchData} isLoading={isLoading}>
+            最後觀測時間:
+            {new Intl.DateTimeFormat("zh-TW", {
+              hour: "numeric",
+              minute: "numeric",
+            }).format(new Date(observationTime))}{" "}
+            {isLoading ? <LoadingIcon /> : <RedoIcon />}
+          </Refresh>
+        </WeatherCard>
+      </Container>
+    </ThemeProvider>
   );
 };
 
